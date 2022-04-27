47eb50ebf6b8b710d60a02862c41dc0829592be2
Anderson <anderson.cardenas.r@gmail.com>
2022-04-27 10:10:46 -0500

    Prod

--- DenunciasAnonimas/DenunciasAnonimas/settings/base.py
+++ DenunciasAnonimas/DenunciasAnonimas/settings/base.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+import json
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,8 +9,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
+
+# Configurar función para obtener el secret
+with open('../secret.json') as file:
+    secret = json.loads(file.read())
+def get_secret(secret_name, secrets=secret):
+    try:
+        return secrets[secret_name]
+    except:
+        msg = "Secret file wasnt found"
+        raise ImportError('Error en carga de secret')
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&&*5hk)0g#730vjjp0$ph2qjd7$zd^-o4+9c%+6!*k+s5r4z6w'
+SECRET_KEY = get_secret('SECRET_KEY')
 ENCRYPT_KEY = b'G2MizlKR65CatILIi929HpbIKV-1OPotdxzdp5Z9GDs='
 
 # Application definition
--- DenunciasAnonimas/DenunciasAnonimas/settings/local.py
+++ DenunciasAnonimas/DenunciasAnonimas/settings/local.py
@@ -17,9 +17,9 @@ ALLOWED_HOSTS = []
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'denuncias',
-        'USER': 'postgres',
-        'PASSWORD': 'anderson',
+        'NAME': get_secret('DB_NAME'),
+        'USER': get_secret('USER'),
+        'PASSWORD': get_secret('PASSWORD'),
         'HOST': 'localhost',
         'PORT': '5432',
     }
